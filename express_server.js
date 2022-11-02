4d7cf5419819a0d72194a7cebcf2f872adfc81f7
Shafaq Saud <shafaq.saud@gmail.com>
2022-11-02 16:33:12 -0400

    stylesheet

--- express_server.js
+++ express_server.js
@@ -9,6 +9,10 @@ const urlDatabase = {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
